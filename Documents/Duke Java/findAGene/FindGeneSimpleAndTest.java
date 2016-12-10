b6f9093566b51e09b3d86b5f53dad19f6eddfc5b
Alessandro Beghi <alessandrobeghi@DTDDAledroBeghi.lan>
2016-12-10 12:27:22 +0100

    findAGene step 1

---
+++ Documents/Duke Java/findAGene/FindGeneSimpleAndTest.java
@@ -0,0 +1,47 @@
+
+/**
+ * Aggiungi qui una descrizione della classe FindGeneSimple
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public class FindGeneSimpleAndTest {
+    public String findGeneSimple(String dna) {
+        // start codon is "ATG"
+        // stop codon is "TAA"
+        String result = "";
+        int startIndex = dna.indexOf("ATG");
+        if (startIndex == -1) //no ATG
+        {
+            return "";
+        }
+        int stopIndex = dna.indexOf("TAA", startIndex+3);
+        if (stopIndex == -1) //no TAA
+        {
+            return "";
+        }
+        result = dna.substring(startIndex, stopIndex+3);
+        
+       
+        return result;
+    }
+    
+    public void testFindGeneSimple() {
+        String dna = "AATGCGTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        String gene = findGeneSimple(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "AATGCTAGGGTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATCCTATGCTTCGGCTGCTCTAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findGeneSimple(dna);
+        System.out.println("Gene is " + gene);
+        
+    }
+
+}
